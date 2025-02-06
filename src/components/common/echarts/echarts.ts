78a1cfa2d4760c0b341d51e413f80e142d5b6599
thelastcookies <thelastcookies@icloud.com>
2025-02-06 19:41:31 +0800

    chore: 引入 ECharts Gauge

--- src/components/common/echarts/echarts.ts
+++ src/components/common/echarts/echarts.ts
@@ -1,5 +1,5 @@
 import * as echarts from 'echarts/core';
-import { BarChart, LineChart, PieChart, RadarChart } from 'echarts/charts';
+import { BarChart, GaugeChart, LineChart, PieChart, RadarChart } from 'echarts/charts';
 import {
   DatasetComponent,
   DataZoomComponent,
@@ -28,6 +28,7 @@ echarts.use([
   LineChart,
   PieChart,
   RadarChart,
+  GaugeChart,
   // Renderer
   CanvasRenderer,
 ]);
